2b35c4f4c3ee62341df38e262f5b5569c7bff554
Muhammad Nurfajri <31876817+powerhero49@users.noreply.github.com>
2023-02-26 11:54:24 +0800

    Update Tugas Final IDN.sh

--- Tugas Final IDN.sh
+++ Tugas Final IDN.sh
@@ -10,7 +10,7 @@ passwd peserta$i <<< "1"$'\n'"1" #menambahkan password
 chmod 755 /home/peserta$i/public_html #merubah hak akses folder public_html
 echo "<h1>Selamat datang di website "peserta$i" <h1>" >> /home/peserta$i/index.html #memasukkan script html di index.html
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/peserta$i.conf #copy file 000-default.conf untuk virtualhost
-sed -i 's/#ServerName www.example.com/ServerName 'peserta$i'.lnxpeserta.33.id/g' /etc/apache2/sites-available/peserta$i.conf #merubah servername
+sed -i 's/#ServerName www.example.com/ServerName 'peserta$i'.lnxpeserta-33.id/g' /etc/apache2/sites-available/peserta$i.conf #merubah servername
 sed -i 's\DocumentRoot /var/www/html\DocumentRoot /home/'peserta$i'\g' /etc/apache2/sites-available/peserta$i.conf #merubah documentroot
 a2ensite peserta$i.conf #enable virtualhost
 done
