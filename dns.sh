95ae45938265091730791308e85a107747886e27
Muhammad Nurfajri <31876817+powerhero49@users.noreply.github.com>
2023-02-26 22:39:57 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -4,7 +4,7 @@ cp /etc/bind/db.127 /etc/bind/db.reverse
 sed -i 's/localhost/lnxpeserta-33.id/g' /etc/bind/db.forward
 sed -i 's/localhost/lnxpeserta-33.id/g' /etc/bind/db.reverse
 echo '@	IN	NS	lnxpeserta-33.id.' >> /etc/bind/db.forward
-echo '@	IN	A	192.168.20.253.' >> /etc/bind/db.forward
+echo '@	IN	A	192.168.20.253' >> /etc/bind/db.forward
 echo '@	IN	NS	lnxpeserta-33.id.' >> /etc/bind/db.reverse
 echo '253	IN	PTR	lnxpeserta-33.id.' >> /etc/bind/db.reverse
 for ((i=1; i<=50; i++))
